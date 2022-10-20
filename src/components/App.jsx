68d9c296e20ffc62071dce2592946122ad6d2a05
Roweshil <x-vdm@hotmail.com>
2022-10-20 00:14:04 -0500

    añadido react

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/global.scss';
+
+const App = () => {
+    return(
+        <h1>Hola Mundo ! curso practico React!!</h1>
+    );
+}
+
+export default App;
